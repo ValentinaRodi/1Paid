8e2a39dac871b9366d6ce8a58334fe1cd6244de4
Родионова Валентина <rodionova.v@x3group.ru>
2023-12-20 16:55:14 +0500

    replace svg on img in Main

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import LendingPage from "./pages/lending/Lending";
 import Registration from "./components/registration/Registration";
-import NewPass from "./components/newpass/Newpass";
 import Newnik from "./components/newnik/Newnik";
 import Recpass from "./components/recpass/Recpass";
 import Authorization from "./components/authorization/Authorization";
@@ -21,7 +20,6 @@ function App() {
         <Route path="/" element={<LendingPage />} />
         <Route path="MainPage" element={<MainPage />} />
         <Route path="Registration" element={<Registration />} />
-        <Route path="NewPass" element={<Newpass />} />
         <Route path="Newnik" element={<Newnik />} />
         <Route path="RecPass" element={<Recpass />} />
         <Route path="Authorization" element={<Authorization />} />
