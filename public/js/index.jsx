02f9216f709c73b30bb7a675e8ceb5f57d401fab
Родионова Валентина <rodionova.v@x3group.ru>
2023-12-25 17:06:15 +0500

    add html  profile

--- public/js/index.jsx
+++ public/js/index.jsx
@@ -4,15 +4,15 @@ import './index.less'
 
 import "./App.less";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import NullMain from './pages/nullMain/NullMain';
+import Profile from './pages/profile/Profile';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<NullMain />} />
-
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   );
--- public/js/landing.jsx
+++ public/js/landing.jsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LendingPage from "./pages/lending/Lending";
 import NullMain from './pages/nullMain/NullMain';
+import Profile from './pages/profile/Profile';
 
 function Landing() {
   return (
@@ -12,6 +13,7 @@ function Landing() {
       <Routes>
         <Route path="/" element={<NullMain />} />
         <Route path="/landing" element={<LendingPage />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   );
@@ -21,6 +23,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Landing />
+      <Profile />
     </BrowserRouter>
   </React.StrictMode>,
 )
