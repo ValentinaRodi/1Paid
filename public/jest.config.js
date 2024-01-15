237ed04dd6b7a8bab4eee319e3b15cd56df28fc1
firstvirus <littlevirus@list.ru>
2024-01-15 07:54:43 +0300

    Clear

--- public/jest.config.js
+++
@@ -1,8 +0,0 @@
-// Sync object
-/** @type {import('@jest/types').Config.InitialOptions} */
-const config = {
-  coverageDirectory: '__test__/**/*.j',
-  collectCoverageFrom: ['!**/node_modules/**'],
-}
-
-module.exports = config
