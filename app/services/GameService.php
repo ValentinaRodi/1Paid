f03d5f1c716b5bb601e7be3f528a6ec5079f4283
DeanMSI <gtgodz1ll@yandex.ru>
2024-04-25 14:50:26 +0300

    fix commented filesV2

--- app/services/GameService.php
+++ app/services/GameService.php
@@ -16,54 +16,54 @@ class GameService
 {
     public static function getList($offset)
     {
-        // $games = Game::find()
-        //         ->limit(8)
-        //         ->offset((int) $offset)
-        //         ->asArray()->all();
+         $games = Game::find()
+                 ->limit(8)
+                 ->offset((int) $offset)
+                 ->asArray()->all();
         $langsIds = [];
         $filesIds = [];
         $gamesIds = [];
-        // foreach ($games as $game) {
-        //     $langsIds[] = $game['lang_id'];
-        //     $filesIds[] = $game['icon_id'];
-        //     $filesIds[] = $game['background_id'];
-        //     $gamesIds[] = $game['id'];
-        // }
+         foreach ($games as $game) {
+             $langsIds[] = $game['lang_id'];
+             $filesIds[] = $game['icon_id'];
+             $filesIds[] = $game['background_id'];
+             $gamesIds[] = $game['id'];
+         }
 
-        // $categories = CategoryService::getListOfGames($gamesIds);
+         $categories = CategoryService::getListOfGames($gamesIds);
         $files = FileService::getFiles($filesIds);
 		$langs = LangService::getLangs($langsIds);
 
-        // foreach ($games as $key => $game) {
-        //     foreach ($langs as $lang) {
-        //         if ($game['lang_id'] === $lang['id']) {
-        //             $games[$key]['name'] = $lang['russian']; // Потом сделать выбор языка.
-        //         }
-        //     }
-        //     foreach ($files as $file) {
-        //         if ($game['icon_id'] === $file['id']) {
-        //             $games[$key]['icon'] = $file['fileName'];
-        //         }
-        //         if ($game['background_id'] === $file['id']) {
-        //             $games[$key]['bg_image'] = $file['fileName'];
-        //         }
-        //     }
-        //     foreach ($categories as $category) {
-        //         if ($category['game_id'] === $game['id']) {
-        //             unset($category['game_id']);
-        //             $games[$key]['categories'][] = $category;
-        //         }
-        //     }
-        //     unset($games[$key]['lang_id']);
-        //     unset($games[$key]['icon_id']);
-        //     unset($games[$key]['background_id']);
-        // }
-        // $count = Game::find()->count();
-        // return [
-        //     'games' => $games,
-        //     'offset' => $offset,
-        //     'count' => $count,
-        //     ];
+         foreach ($games as $key => $game) {
+             foreach ($langs as $lang) {
+                 if ($game['lang_id'] === $lang['id']) {
+                     $games[$key]['name'] = $lang['russian']; // Потом сделать выбор языка.
+                 }
+             }
+             foreach ($files as $file) {
+                 if ($game['icon_id'] === $file['id']) {
+                     $games[$key]['icon'] = $file['fileName'];
+                 }
+                 if ($game['background_id'] === $file['id']) {
+                     $games[$key]['bg_image'] = $file['fileName'];
+                 }
+             }
+             foreach ($categories as $category) {
+                 if ($category['game_id'] === $game['id']) {
+                     unset($category['game_id']);
+                     $games[$key]['categories'][] = $category;
+                 }
+             }
+             unset($games[$key]['lang_id']);
+             unset($games[$key]['icon_id']);
+             unset($games[$key]['background_id']);
+         }
+         $count = Game::find()->count();
+         return [
+             'games' => $games,
+             'offset' => $offset,
+             'count' => $count,
+             ];
     }
 
     public static function checkGameID($gameId)
